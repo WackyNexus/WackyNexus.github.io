92a069d90761321a4498c28a05bd14484a57e1db
WackyWormer <WackyWormer@hotmail.com>
2020-07-04 23:03:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var wpm = (function(wpm) {
   "use strict"
-  var wpm.index = (function(index) {
+  wpm.index = (function(index) {
     ow_Write("#output_main", "Loading wpm.index");
     return {
       test: "Hello world!"
