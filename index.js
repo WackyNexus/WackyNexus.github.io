403a59dbe92d37c3f673cc909b3b35453b4c76df
WackyWormer <WackyWormer@hotmail.com>
2020-07-04 22:57:43 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var wpm = (function(wpm) {
+  "use strict"
+  var wpm.index = (function(index) {
+    ow_Write("#output_main", "Loading wpm.index");
+    return {
+      test: "Hello world!"
+    };
+  }(wpm.index));
+}(wpm));
